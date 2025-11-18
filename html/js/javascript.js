02d231910ce11fb9d63dee89d891232ea4fed76e
Steven <127scastropacheco@frhsd.com>
2025-11-18 13:58:37 -0500

    ;lkjhgf

---
+++ html/js/javascript.js
@@ -0,0 +1,30 @@
+/** @format */
+//let pfForm = document.getElementById("profileForm");
+//
+let urName = document.getElementById("nameInput");
+//
+let urAge = document.getElementById("ageInput");
+//
+let colorDrop = document.getElementById("colorInput");
+//
+//let elButton = document.getElementById("button");
+//
+let submitButton = document.getElementById("submitBtn");
+//
+let raDio = document.querySelectorAll("input[type='radio']");
+//
+//let comesOut = document.getElementById("outPut");
+//
+///const element = document.getElementById(submitBtn);
+//element.addEventListener("click", getInfo);
+
+//function getInfo() {
+// console.log(naquan);
+//}
+
+function button() {
+  let inputName = document.getElementById("nameInput");
+
+  let value = inputName.value;
+  alert("Input value:" + value);
+}
