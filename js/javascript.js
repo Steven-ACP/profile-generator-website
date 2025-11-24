fc2e1db1a89e89678a2c5df4c43f0aeb99784860
Steven <127scastropacheco@frhsd.com>
2025-11-24 13:55:24 -0500

    g;.jl

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 /** @format */
-//let pfForm = document.getElementById("profileForm");
+
 //
 let urName = document.getElementById("nameInput");
 //
@@ -7,20 +7,10 @@ let urAge = document.getElementById("ageInput");
 //
 let colorDrop = document.getElementById("colorInput");
 //
-//let elButton = document.getElementById("button");
-//
 let submitButton = document.getElementById("submitBtn");
 //
 let raDio = document.querySelectorAll("input[type='radio']");
 //
-//let comesOut = document.getElementById("outPut");
-//
-///const element = document.getElementById(submitBtn);
-//element.addEventListener("click", getInfo);
-
-//function getInfo() {
-// console.log(naquan);
-//}
 
 submitButton.addEventListener("click", button);
 
@@ -43,14 +33,26 @@ function button() {
   let inputName = document.getElementById("nameInput");
   let inputAge = document.getElementById("ageInput");
   let inputColor = document.getElementById("colorInput");
+  let inputRadio = document.getElementById("input[type='radio']");
+  /////////////////////////
   let value = inputName.value;
   let ageValue = inputAge.value;
   let colorValue = inputColor.value;
+  let radioValue = inputRadio.value;
   document.getElementById("outPut").textContent =
     "Your Name: " +
     value +
     "      Your Age: " +
     ageValue +
     "         Favorite Color: " +
-    colorValue;
+    colorValue +
+    "Your favorite animal" +
+    radioValue;
+}
+
+const names = ["Dog", "Cat", "Fish"];
+
+for (let i = 0; i < names.length; i++) {
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
 }
